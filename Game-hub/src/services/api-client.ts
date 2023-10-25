03b6ac6348605646a994083842c3724daee300fa
ajabdi <libaaxasad631@gmail.com>
2023-10-25 12:05:20 +0100

    api key changed

--- Game-hub/src/services/api-client.ts
+++ Game-hub/src/services/api-client.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'c1c99b51ea46486c871a00b929d811d0'
+    key: 'b8c9379b7c21463494915f8334c4f7d2'
   }
 })
