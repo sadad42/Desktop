04cad45e0ccdc4d9f7d93944190a817f19814c57
ajabdi <libaaxasad631@gmail.com>
2024-01-08 16:39:55 +0000

    welcome screen made

--- donewithit/App.js
+++ donewithit/App.js
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to my first project</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WelcomeScreen/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
